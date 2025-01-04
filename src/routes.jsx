e4d59283a37918ef263805434fdc77db386c93f0
vishnuKumar3 <narayanavishnukumar@gmail.com>
2025-01-04 19:30:58 +0530

    routes changed

--- src/routes.jsx
+++ src/routes.jsx
@@ -5,7 +5,7 @@ export default function Routes(){
     return(
         useRoutes([
             {
-                path:'/home',
+                path:'/',
                 element:<Home/>
             }
         ])
